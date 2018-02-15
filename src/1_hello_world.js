493f15cb72979b3af00efcf446f7411503fb02e6
artfish88 <arthur.rybak@gmail.com>
2018-02-15 16:14:40 +0200

    adding images exchange

--- src/1_hello_world.js
+++ src/1_hello_world.js
@@ -1,3 +1,21 @@
-/**
- * Created by arturr on 1/25/2018.
- */
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+    res.sendfile('src/html/1_hello_world.html');
+});
+
+//Whenever someone connects this gets executed
+io.on('connection', function(socket) {
+    console.log('A user connected', socket.id);
+
+    //Whenever someone disconnects this piece of code executed
+    socket.on('disconnect', function () {
+        console.log('A user disconnected');
+    });
+});
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
